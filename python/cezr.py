670c63e38dfe1ee1c8dc120c07f806b3a76c4f99
Twentytwo44 <natchapon.ponl@kmutt.ac.th>
2023-07-31 16:30:20 +0700

    cezarcode

--- python/cezr.py
+++ python/cezr.py
@@ -1,4 +1,4 @@
-def endcodding(msg):
+def encoding(msg):
 
     num = 20
 
@@ -13,7 +13,9 @@ def endcodding(msg):
             newlist.append(chr(cznum))       
         else:
             newlist.append(chr(cezr))
-        
+
+    decoded_msg = "".join(newlist)
+    print(decoded_msg)
 
 
     print(newlist)
@@ -37,5 +39,26 @@ def decoding(msg):
     print(newlist)
 
 
-endcodding('WELCOME')
-decoding('CJQHTRJ')
+encoding('WELCOME')
+def decoding_last(msg, num):
+    newlist = []
+    for i in range(len(msg)):
+        newmsg = ord(msg[i])
+        cezr = newmsg + num
+        if cezr > 90:
+            z = cezr - 90
+            cznum = 65 + z
+            newlist.append(chr(cznum))
+        else:
+            newlist.append(chr(cezr))
+
+    decoded_msg = "".join(newlist)
+    return decoded_msg
+
+# Example usage:
+message = "CJQHTRJ"
+shift_amount = 20
+decoded_message = decoding_last(message, shift_amount)
+print(decoded_message)
+
+encoding('WELCOMETOACSFIRSTMEET')
