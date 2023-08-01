7ed4c21760588e4f78147267edd659018d80dc7b
Narutchai <koonfluk2015@gmail.com>
2023-08-02 00:02:33 +0700

    debug

--- client/src/components/Showword.tsx
+++ client/src/components/Showword.tsx
@@ -1,31 +1,47 @@
 import { dataProps } from "./interface";
 
 
-const Showword = ({some,num}:dataProps) => {
-      
+const Showword = ({ some, num }: dataProps) => {
+
     function decodingLast(msg: string, num: number): string {
         const newlist: string[] = [];
-    
+
         for (let i = 0; i < msg.length; i++) {
-            const newmsg: number = msg.charCodeAt(i);
-            let cezr: number = newmsg + num;
-            if (cezr > 90) {
-                const z: number = cezr - 90;
-                const cznum: number = 65 + z;
-                newlist.push(String.fromCharCode(cznum));
+            const newmsg = msg.charCodeAt(i);
+            let cezr: number;
+
+            if (num > 0) {
+                cezr = newmsg + num;
+                if (cezr > 90) {
+                    const z = cezr - 90;
+                    const cznum = 64 + z;
+                    newlist.push(String.fromCharCode(cznum));
+                } else {
+                    newlist.push(String.fromCharCode(cezr));
+                }
+            } else if (num < 0) {
+                cezr = newmsg - num;
+                if (cezr > 90) {
+                    const z = cezr - 90;
+                    const cznum = 64 + z;
+                    newlist.push(String.fromCharCode(cznum));
+                } else {
+                    newlist.push(String.fromCharCode(cezr));
+                }
             } else {
-                newlist.push(String.fromCharCode(cezr));
+                newlist.push(msg[i]);
             }
         }
-    
-        const decoded_msg: string = newlist.join('');
+
+        const decoded_msg = newlist.join('');
         return decoded_msg;
     }
-    
+
     // Example usage:
     const decodedMessage: string = decodingLast(some, num);
     // console.log(decodedMessage);
-    
+
+
     return (
         <>
             <p>{decodedMessage}</p>
